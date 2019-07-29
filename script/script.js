08d0767f97c56c6281186c9295ee63c00195ab61
Shak <ismailshak94@Ismails-MacBook-Air.local>
2019-07-29 11:14:29 -0400

    removes unwanted files

--- script/script.js
+++
@@ -1,8 +0,0 @@
-// const projectIcons = document.querySelectorAll('.project-icon')
-
-// for(let i = 0; i < projectIcons.length; i++) {
-//     projectIcons[i].addEventListener('click', e => {
-//         e.preventDefault();
-
-//     })
-// }
