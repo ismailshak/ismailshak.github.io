263774443aef285b16cb883ef0b95b6d5ec7ed0a
Shak <ismailshak94@Ismails-MacBook-Air.local>
2019-07-29 11:03:11 -0400

    adds pie chart, adds buttons on projects to redirect to repo or deployed site

---
+++ script/script.js
@@ -0,0 +1,8 @@
+// const projectIcons = document.querySelectorAll('.project-icon')
+
+// for(let i = 0; i < projectIcons.length; i++) {
+//     projectIcons[i].addEventListener('click', e => {
+//         e.preventDefault();
+
+//     })
+// }
